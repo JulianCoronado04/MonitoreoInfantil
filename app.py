fa73c5ebff0270092ba3b87a87965ea00cbbbd4b
JulianCoronado04 <juliandcavanzo@gmail.com>
2025-11-17 23:10:21 -0500

    UI: eliminar formulario de búsqueda y mostrar solo Power BI en /herramienta

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
@@ -7,13 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/herramienta', methods=['GET', 'POST'])
+@app.route('/herramienta')
 def herramienta():
     powerbi_url = os.environ.get('POWERBI_EMBED_URL', 'https://app.powerbi.com/view?r=eyJrIjoiNWM4ZDAwYTAtNjJjZi00ZGRkLWJiZGUtMTc5YWI3ZTRiOTJjIiwidCI6IjA3ZGE2N2EwLTFmNDMtNGU4Yy05NzdmLTVmODhiNjQ3MGVlNiIsImMiOjR9')
-    if request.method == 'POST':
-        id_number = request.form.get('id_number', '').strip()
-        return render_template('herramienta.html', id_number=id_number, powerbi_url=powerbi_url)
-    return render_template('herramienta.html', id_number=None, powerbi_url=powerbi_url)
+    return render_template('herramienta.html', powerbi_url=powerbi_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
